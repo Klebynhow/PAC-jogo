18c76ebe813e3e17f423ab9bf269a5720f10f7e4
Klebynhow <vini020304@hotmail.com>
2026-05-27 10:53:22 -0300

    add states em hand

--- scenes/card_ui/card_states/card_released_state.gd
+++ scenes/card_ui/card_states/card_released_state.gd
@@ -1,6 +1,20 @@
 extends CardState
 
+var played: bool
+
 func enter() -> void:
 	card_ui.color.color = Color.DARK_VIOLET
 	card_ui.state.text = "RELEASED"
 	
+	played = false	
+	
+	if not card_ui.targets.is_empty():
+		played = true
+		print("target(s) carta ", card_ui.targets)
+	
+
+func on_input(_event: InputEvent) -> void:
+	if played:
+		return
+		
+	transition_requested.emit(self, CardState.State.BASE)
