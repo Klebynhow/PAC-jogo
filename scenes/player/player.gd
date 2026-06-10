36f78bc16675b8da6de653a20e8eaa5a5fda5bf5
Klebynhow <vini020304@hotmail.com>
2026-06-10 16:43:41 -0300

    add player animation

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -21,6 +21,7 @@ func update_player() -> void:
 	if not is_inside_tree():
 		await ready
 	sprite_2d.texture = stats.art
+	$PlayerAnimation.play("idle")
 	update_stats()
 	
 func update_stats() -> void:
